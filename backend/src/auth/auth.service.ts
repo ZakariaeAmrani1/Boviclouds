cd47dd5689a93f606d6902d104146842069787fa
Naime5_Br <lbidjouhk8@gmail.com>
2025-07-21 16:15:25 +0100

    Naime- Improve authentication and authorization!

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -3,54 +3,52 @@ import {
   ConflictException,
   UnauthorizedException,
 } from '@nestjs/common';
-
-import { LoginDto } from './dto/login.dto';
+import { LoginDto } from './dto/users/login.dto';
 import { InjectModel } from '@nestjs/mongoose';
-import { User } from 'src/user/schemas/user.schema';
+import { User, UserMethods } from 'src/user/schemas/users/user.schema';
 import { Model } from 'mongoose';
-import * as bcrypt from 'bcrypt';
-
 import { JwtService } from '@nestjs/jwt';
-import { CreateUserDto } from './dto/register.dto';
+import { CreateUserDto } from './dto/users/register.dto';
+import { AccountStatus } from 'src/user/schemas/users/user.acc.status';
 
 @Injectable()
 export class AuthService {
   UserService: any;
   constructor(
-    @InjectModel(User.name) private userModel: Model<User>,
-    private jwtService: JwtService,
+    @InjectModel(User.name) private readonly userModel: Model<User,{},UserMethods>,
+    private readonly jwtService: JwtService,
   ) {}
 
   async register(dto: CreateUserDto) {
     const userExists = await this.userModel.findOne({ email: dto.email });
     if (userExists) throw new ConflictException('Email already exists');
-
-    const createdUser = new this.userModel({
-      ...dto,
-
-      date_creation: new Date(),
-    });
-
-    const user = await createdUser.save();
-    const token = this.jwtService.sign({ sub: user._id, role: user.role });
-    return { token, user };
+    const user = await new this.userModel(dto).save();
+    return {
+      status: 'success',
+      message: 'user registred successfully',
+      data: { user },
+    };
   }
 
   async login(dto: LoginDto) {
-    const user = await this.userModel.findOne({ email: dto.email });
-    if (!user || !(await bcrypt.compareSync(dto.password, user.passwordHash))) {
-      throw new UnauthorizedException('Invalid credentials');
-    }
-
-    const token = this.jwtService.sign({ sub: user._id, role: user.role });
-    return { token, user };
-  }
-  async validateUser(email: string, pass: string): Promise<any> {
-    const user = await this.UserService.findByEmail(email);
-    if (!user || user.metadata.statut !== 'valide') return null;
-
-    const isMatch = await bcrypt.compare(pass, user.password);
-    if (isMatch) return user;
-    return null;
+    const user = await this.userModel
+      .findOne({ email: dto.email })
+      .select('+passwordHash');
+      // console.log(user);
+    if (!user || !(await user.correctPassword(dto.password))) {
+        throw new UnauthorizedException('Invalid credentials');
+      }
+    if(!user || user.metadata?.statut !== AccountStatus.APPROVED)
+      throw new UnauthorizedException("Account not approved");
+    const access_token = this.jwtService.sign({
+      sub: user._id,
+      email: user.email,
+      role: user.role,
+    });
+    return {
+      status: 'success',
+      message: 'user logged in successfully',
+      data: { access_token, user },
+    };
   }
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import morgan from 'morgan';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 9001);
-  if (process.env.ENVIRENEMMENT=="developpement"){
-    app.use(morgan)
+  app.useGlobalPipes(new ValidationPipe());
+  // app.useGlobalGuards(new RolesGuard(app.get(Reflector)));
+  await app.listen(process.env.PORT ?? 3000);
+  if (process.env.ENV === "developpement"){
+    app.use(morgan('dev'))
   }
 }
 bootstrap();
