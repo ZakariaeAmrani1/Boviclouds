07c79c46ff3cd33adb1bbfdec597f0063d052edc
Zakariae Amrani <99502909+Alexo05@users.noreply.github.com>
2025-07-23 12:57:24 +0100

    feat: Add CORS support, registration functionality, and Axios dependency

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,16 +4,16 @@ import * as morgan from 'morgan';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   // app.useGlobalGuards(new RolesGuard(app.get(Reflector)));
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
-  if (process.env.ENV === "developpement"){
-    app.use(morgan('dev'))
+  if (process.env.ENV === 'developpement') {
+    app.use(morgan('dev'));
   }
 }
 bootstrap();
-
-
-
