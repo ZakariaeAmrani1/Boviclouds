4dfa70252fc840ff3fee226c51e6dae0445d0810
Naime5_Br <lbidjouhk8@gmail.com>
2025-07-23 17:52:30 +0100

    Naime- Adding a base insemination logic

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as morgan from 'morgan';
 import { ValidationPipe } from '@nestjs/common';
+import { AllExceptionsFilter } from './filters/all-exceptions.filter';
 
 async function bootstrap() {
 
@@ -12,9 +13,11 @@ async function bootstrap() {
     forbidNonWhitelisted:true
   }));
   // app.useGlobalGuards(new RolesGuard(app.get(Reflector)));
+  app.useGlobalFilters(new AllExceptionsFilter());
   await app.listen(process.env.PORT ?? 3000);
-  if (process.env.ENV === "developpement"){
-    app.use(morgan('dev'))
+  console.log(process.env.NODE_ENV);
+  if (process.env.NODE_ENV === 'developement') {
+    app.use(morgan('dev'));
   }
 }
 bootstrap();
