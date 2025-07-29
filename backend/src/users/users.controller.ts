49903b03cfd1002514d0126d2288a2a1b8c407ef
Naime5_Br <lbidjouhk8@gmail.com>
2025-07-29 11:26:25 +0100

    naime -users completion

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Patch,Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch,Post, Req, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { CreateUserDto } from 'src/auth/dto/users/register.dto';
@@ -6,54 +6,58 @@ import { UpdateUserDto } from './dtos/update-user.dto';
 import { CurrentUser } from 'src/auth/decorators/active-user.decorator';
 import { UpdatePwdDto } from './dtos/update-pwd.dto';
 
-@Controller('api/v1/users')
-export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  @UseGuards(JwtAuthGuard)
+  @Controller('api/v1/users')
+  export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
 
-  @UseGuards(JwtAuthGuard)
-  @Get()
-  async getAllUsers(@CurrentUser() user) {
-    console.log(user);
-    return this.usersService.findAll();
-  }
+    @Get('me')
+    async getCurrentUser(@CurrentUser() user) {
+      return this.getUser(user.userId);
+    }
 
-  @UseGuards(JwtAuthGuard)
-  @Get(':id')
-  async getUser(@Param('id') id: string) {
-    return this.usersService.findById(id);
-  }
 
-  @UseGuards(JwtAuthGuard)
-  @Patch(':id')
-  async updateUser(
-    @Param('id') id: string,
-    @Body() updateUserDto: UpdateUserDto,
-  ) {
-    return await this.usersService.updateUser(id, updateUserDto);
-  }
+    @Get()
+    async getAllUsers() {
+      return this.usersService.findAll();
+    }
 
-  @UseGuards(JwtAuthGuard)
-  @Get(':id')
-  async deleteUser(@Param('id') id: string) {
-    return this.usersService.deleteUser(id);
-  }
-  @UseGuards(JwtAuthGuard)
-  @Post('forgot-password')
-  async forgotPassword(@Body('email') email: string, @Req() req) {
-    return this.usersService.forgotPassword(email, req);
-  }
-  @UseGuards(JwtAuthGuard)
-  @Post('reset-password/:token')
-  async resetPassword(
-    @Param('token') token: string,
-    @Body() dto: UpdatePwdDto,
-  ) {
-    return this.usersService.resetPassword(token, dto);
-  }
 
-  @UseGuards(JwtAuthGuard)
-  @Post('demande')
-  async requestAccount(@Body() dto: CreateUserDto) {
-    return this.usersService.requestAccount(dto);
+    @Get(':id')
+    async getUser(@Param('id') id: string) {
+      return this.usersService.findById(id);
+    }
+
+
+    @Patch(':id')
+    async updateUser(
+      @Param('id') id: string,
+      @Body() updateUserDto: UpdateUserDto,
+    ) {
+      return await this.usersService.updateUser(id, updateUserDto);
+    }
+
+    @Delete(':id')
+    async deleteUser(@Param('id') id: string) {
+      return this.usersService.deleteUser(id);
+    }
+
+    @Post('forgot-password')
+    async forgotPassword(@Body('email') email: string, @Req() req) {
+      return this.usersService.forgotPassword(email, req);
+    }
+
+    @Post('reset-password/:token')
+    async resetPassword(
+      @Param('token') token: string,
+      @Body() dto: UpdatePwdDto,
+    ) {
+      return this.usersService.resetPassword(token, dto);
+    }
+
+
+    @Post('demande')
+    async requestAccount(@Body() dto: CreateUserDto) {
+      return this.usersService.requestAccount(dto);
+    }
   }
-}
