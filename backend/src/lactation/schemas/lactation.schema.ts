206b3ee2d41856695ea41c209a93d29d314e69fc
Zakariae Amrani <99502909+Alexo05@users.noreply.github.com>
2025-07-31 15:58:12 +0100

    commit20

--- backend/src/lactation/schemas/lactation.schema.ts
+++ backend/src/lactation/schemas/lactation.schema.ts
@@ -1,13 +1,11 @@
-import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-import { HydratedDocument, Types } from "mongoose";
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument, Query, Types } from 'mongoose';
 
 export type LactationDocument = HydratedDocument<Lactation>;
-
 
 @Schema({ timestamps: true })
 export class Lactation {
   @Prop({ type: Types.ObjectId, required: true, ref: 'Identification' })
-
   sujet_id: Types.ObjectId;
 
   @Prop({ required: true })
@@ -29,23 +27,18 @@ export class Lactation {
   pct_mg: number;
 
   @Prop({ type: Types.ObjectId, required: true, ref: 'User' })
-
   controleur_laitier_id: Types.ObjectId;
 }
 
 export const LactationSchema = SchemaFactory.createForClass(Lactation);
 
-LactationSchema.pre(/^find/,function(this:Query<any,Lactation>,next){
-  this
-  .populate({
+LactationSchema.pre(/^find/, function (this: Query<any, Lactation>, next) {
+  this.populate({
     path: 'controleur_laitier_id',
     select: 'CIN name email phone role',
-  })
-  .populate({
+  }).populate({
     path: 'sujet_id',
-    select: 'infos_sujet'
+    select: 'infos_sujet',
   });
   next();
-})
-
-
+});
